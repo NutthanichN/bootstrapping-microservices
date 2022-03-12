fe74785116997288a281fbcd680151aecf1ccc1b
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-03-12 13:50:38 +0700

    [ch2] Add an endpoint for streaming a video from a local filesystem to a web browser

--- video-streaming-server/index.js
+++ video-streaming-server/index.js
@@ -1,10 +1,32 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Welcome to FlixTube");
 });
+
+app.get("/video", (req, res) => {
+  const path = "../videos/SampleVideo_1280x720_1mb.mp4";
+
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.error("An error occurred");
+      res.sendStatus(500);
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+
+    // stream video to the web browser
+    fs.createReadStream(path).pipe(res);
+  });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
