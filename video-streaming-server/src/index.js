8bc28695abe925341142ba1e5a49a18785cb2d06
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-03-16 16:36:27 +0700

    [ch5] Refactor and modify the video-streaming service to send a post request to the history service

--- video-streaming-server/src/index.js
+++ video-streaming-server/src/index.js
@@ -10,6 +10,14 @@ if (!process.env.PORT) {
   throw new Error("Please specify the port number for the HTTP server with the environment variable PORT");
 }
 
+if (!process.env.DBHOST) {
+  throw new Error("Please specify the databse host using environment variable DBHOST.");
+}
+
+if (!process.env.DBNAME) {
+  throw new Error("Please specify the name of the database using environment variable DBNAME");
+}
+
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
@@ -18,56 +26,104 @@ const DBNAME = process.env.DBNAME;
 
 console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
-function main() {
-  return mongodb.MongoClient.connect(DBHOST) // Connect to the database.
+// send request to the history service
+function sendViewedMessage(videoPath) {
+  const postOptions = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+  };
+
+  const requestBody = {
+      videoPath: videoPath
+  };
+
+  const req = http.request(
+      "http://history/viewed",
+      postOptions
+  );
+
+  req.on("close", () => {
+      console.log("Sent 'viewed' message to history microservice.");
+  });
+
+  req.on("error", (err) => {
+      console.error("Failed to send 'viewed' message!");
+      console.error(err && err.stack || err);
+  });
+
+  req.write(JSON.stringify(requestBody));
+  req.end();
+}
+
+function setupHandlers(app, db) {
+
+  const videosCollection = db.collection("videos");
+
+  app.get("/", (req, res) => {
+    res.send("Welcome to FlixTube");
+  });
+
+  app.get("/video", (req, res) => {
+    const videoId = new mongodb.ObjectID(req.query.id);
+    videosCollection.findOne({ _id: videoId })
+        .then(videoRecord => {
+            if (!videoRecord) {
+                res.sendStatus(404);
+                return;
+            }
+
+            console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}.`);
+
+            const forwardRequest = http.request( // Forward the request to the video storage microservice.
+                {
+                    host: VIDEO_STORAGE_HOST,
+                    port: VIDEO_STORAGE_PORT,
+                    path:`/video?path=${videoRecord.videoPath}`, // Video path now retrieved from the database.
+                    method: 'GET',
+                    headers: req.headers
+                },
+                forwardResponse => {
+                    res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+                    forwardResponse.pipe(res);
+                }
+            );
+
+            req.pipe(forwardRequest);
+            sendViewedMessage(videoRecord.videoPath)  // send request to history service
+        })
+        .catch(err => {
+            console.error("Database query failed.");
+            console.error(err && err.stack || err);
+            res.sendStatus(500);
+        });
+  });
+}
+
+function startHttpServer(db) {
+  return new Promise(resolve => {
+      const app = express();
+      setupHandlers(app, db);
+
+      app.listen(PORT, () => {
+        console.log(`Microservice listening at port ${PORT}, please load the data file db-fixture/videos.json into your database before testing this microservice.`);
+        resolve();
+      });
+  });
+}
+
+function connectDb() {
+  return mongodb.MongoClient.connect(DBHOST)
       .then(client => {
-          const db = client.db(DBNAME);
-          const videosCollection = db.collection("videos");
+          return client.db(DBNAME);
+      });
+}
 
-          app.get("/", (req, res) => {
-            res.send("Welcome to FlixTube");
-          });
-
-          app.get("/video", (req, res) => {
-              const videoId = new mongodb.ObjectID(req.query.id);
-              videosCollection.findOne({ _id: videoId })
-                  .then(videoRecord => {
-                      if (!videoRecord) {
-                          res.sendStatus(404);
-                          return;
-                      }
-
-                      console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}.`);
-
-                      const forwardRequest = http.request( // Forward the request to the video storage microservice.
-                          {
-                              host: VIDEO_STORAGE_HOST,
-                              port: VIDEO_STORAGE_PORT,
-                              path:`/video?path=${videoRecord.videoPath}`, // Video path now retrieved from the database.
-                              method: 'GET',
-                              headers: req.headers
-                          },
-                          forwardResponse => {
-                              res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-                              forwardResponse.pipe(res);
-                          }
-                      );
-
-                      req.pipe(forwardRequest);
-                  })
-                  .catch(err => {
-                      console.error("Database query failed.");
-                      console.error(err && err.stack || err);
-                      res.sendStatus(500);
-                  });
-          });
-
-          //
-          // Starts the HTTP server.
-          //
-          app.listen(PORT, () => {
-              console.log(`Microservice listening at port ${PORT}, please load the data file db-fixture/videos.json into your database before testing this microservice.`);
-          });
+function main() {
+  return connectDb(DBHOST)
+      .then(db => {
+          return startHttpServer(db);
       });
 }
 
