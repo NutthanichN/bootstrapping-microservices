6a3c6ed8b23474a6252add997ff686e9f9264c2a
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-03-16 14:16:34 +0700

    [ch4] Add db container to docker compose file and modify video-streaming service to get video data from the database

--- video-streaming-server/src/index.js
+++ video-streaming-server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const dotenv = require("dotenv");
+const mongodb = require("mongodb");
 
 const app = express();
 dotenv.config();
@@ -12,30 +13,67 @@ if (!process.env.PORT) {
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to FlixTube");
-});
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
-app.get("/video", (req, res) => {
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: '/video?path=SampleVideo_1280x720_1mb.mp4',
-      method: 'GET',
-      headers: req.headers,
-    },
-    forwardResponse => {
-      res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-      forwardResponse.pipe(res);
-    }
-  );
+function main() {
+  return mongodb.MongoClient.connect(DBHOST) // Connect to the database.
+      .then(client => {
+          const db = client.db(DBNAME);
+          const videosCollection = db.collection("videos");
 
-  req.pipe(forwardRequest);
+          app.get("/", (req, res) => {
+            res.send("Welcome to FlixTube");
+          });
 
-})
+          app.get("/video", (req, res) => {
+              const videoId = new mongodb.ObjectID(req.query.id);
+              videosCollection.findOne({ _id: videoId })
+                  .then(videoRecord => {
+                      if (!videoRecord) {
+                          res.sendStatus(404);
+                          return;
+                      }
 
-app.listen(PORT, () => {
-  console.log(`Microservice online, listening on port ${PORT}!`);
-});
+                      console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}.`);
+
+                      const forwardRequest = http.request( // Forward the request to the video storage microservice.
+                          {
+                              host: VIDEO_STORAGE_HOST,
+                              port: VIDEO_STORAGE_PORT,
+                              path:`/video?path=${videoRecord.videoPath}`, // Video path now retrieved from the database.
+                              method: 'GET',
+                              headers: req.headers
+                          },
+                          forwardResponse => {
+                              res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+                              forwardResponse.pipe(res);
+                          }
+                      );
+
+                      req.pipe(forwardRequest);
+                  })
+                  .catch(err => {
+                      console.error("Database query failed.");
+                      console.error(err && err.stack || err);
+                      res.sendStatus(500);
+                  });
+          });
+
+          //
+          // Starts the HTTP server.
+          //
+          app.listen(PORT, () => {
+              console.log(`Microservice listening at port ${PORT}, please load the data file db-fixture/videos.json into your database before testing this microservice.`);
+          });
+      });
+}
+
+main()
+  .then(() => console.log("Microservice online."))
+  .catch(err => {
+      console.error("Microservice failed to start.");
+      console.error(err && err.stack || err);
+  });
