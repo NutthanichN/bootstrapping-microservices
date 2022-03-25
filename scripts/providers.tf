c6d8eb8305708135f979d18e436346845d53db76
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-03-26 00:19:58 +0700

    [ch7] Add tf scripts for deploying database and RabbitMQ to Kubernetes cluster

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -6,10 +6,22 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+}
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
 }
